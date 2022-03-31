2408ad1334776cd24dab5488e7ad840d4982a131
Kyle Nissley <knissley97@gmail.com>
2022-03-31 13:59:32 -0700

    Add babel and configure webpack

--- client/src/components/styled/FlexContainer.styled.js
+++ client/src/components/styled/FlexContainer.styled.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const FlexContainer = styled.div`
   display: flex;
   flex-direction: ${(props) => props.direction};
-  gap: ${(props) => props.gap};
+  gap: ${(props) => props.gap || '3em'};
   background-color: #f3f3f3;
 `;
 
