4301d5f4cf4ebc7c0a717c56e5b25bcecfc3d945
JoeVanCamp <vancampjoe@gmail.com>
2022-03-31 15:49:26 -0700

    Create a test component for code review

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Button from './styled/Button.styled';
 import FlexContainer from './styled/FlexContainer.styled';
 import Avatar from './styled/Avatar.styled';
+import Test from './TestComponent';
 
 const App = () => {
   console.log('hello world');
@@ -11,6 +12,7 @@ const App = () => {
       <FlexContainer direction="column" gap="5em">
         Hello World!
         <Button>Click Me</Button>
+        <Test />
         <Avatar />
       </FlexContainer>
     </div>
