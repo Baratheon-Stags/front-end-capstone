d52d555d76c32c5ce766727f60acac06d09f68ff
JoeVanCamp <vancampjoe@gmail.com>
2022-04-05 21:37:56 -0700

    Removed Modal prototype component

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,14 +3,18 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { regular, solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 import { CardContainer, CardImage, CardDesc } from './styled/Card.styled';
-import GenerateStarRatings from './generateStarRatings';
-import ComparisonModal from './ComparisonModal';
+import GenerateStarRatings from './GenerateStarRatings';
 
 import FlexContainer from './styled/FlexContainer.styled';
 
 const Card = ({ product }) => {
-  const { image, category, name, default_price, ratings } = product;
-  const [showModal, setShowModal] = useState(false);
+  const { id, image, category, name, default_price, ratings } = product;
+
+  // Get features for current item
+  // Store them somewhere in state
+  // Get features for each Related item
+  // Load features of Related into an array
+  // Populate modals for each Card
 
   const style = {
     position: 'relative',
@@ -25,12 +29,10 @@ const Card = ({ product }) => {
   const openComparison = (event) => {
     event.stopPropagation();
     console.log('Opening modal..');
-    setShowModal((prev) => !prev);
   };
 
   return (
     <li>
-      <ComparisonModal showModal={showModal} setShowModal={setShowModal} />
       <CardContainer onClick={goToProduct}>
         <CardImage url={image}>
           <FontAwesomeIcon onClick={openComparison} style={style} icon={regular('star')} className="fa-lg" />
