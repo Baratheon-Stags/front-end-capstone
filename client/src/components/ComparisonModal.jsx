3d276ba1f7a53d7e1514e043b776dbc1cddcdacf
JoeVanCamp <vancampjoe@gmail.com>
2022-04-05 19:41:06 -0700

    Updated Comparison Modal to lock to certain part of the page

--- client/src/components/ComparisonModal.jsx
+++ client/src/components/ComparisonModal.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
+import FlexContainer from './styled/FlexContainer.styled';
 
 const StyledBackground = styled.div`
-  width: 200px;
-  height: 200px;
+  width: 400px;
+  height: 400px;
+  top: 100%;
+  left: 50%;
+  box-shadow: 0 5px 15px rgba(0, 0, 0, 0.2);
+  display: flex;
+  flex-direction: ${(props) => props.direction || 'row'};
+  position: absolute;
   border: 2px solid black;
-  background-color: E39525;
+  background: white;
+
 `;
 
-const ComparisonModal = ({ showModal, setShowModal }) => {
+const CompareBar = styled.div`
+  font-weight: bolder;
+  margin: 0.5em;
+  flex-direction: ${(props) => props.direction || 'row'};
+`;
 
-  return (
-    <>
-    {showModal ? <StyledBackground/>: null}
-    </>
-  )
-}
+const ComparisonModal = ({ showModal, setShowModal }) => (
+  <>
+    {showModal
+      ? (
+        <StyledBackground direction="column">
+          <CompareBar direction="column">Comparing</CompareBar>
+          <FlexContainer justify="space-between" direction="row">
+            <CompareBar>Item 1</CompareBar>
+            <CompareBar>Item 2</CompareBar>
+          </FlexContainer>
+
+        </StyledBackground>
+      )
+      : null}
+  </>
+);
 
 export default ComparisonModal;
