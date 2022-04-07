eb806a3e8861a6968d6b67a5bd9c8b95113a4c13
Kyle Nissley <knissley97@gmail.com>
2022-04-07 14:03:27 -0500

    Adjust gallery spacing

--- client/src/components/overview/GalleryCarousel.jsx
+++ client/src/components/overview/GalleryCarousel.jsx
@@ -43,6 +43,10 @@ const CarouselContent = styled.div`
     aspect-ratio: 9/16;
     border-radius: 2px;
   }
+
+  & > img:hover {
+    cursor: zoom-in;
+  }
 `;
 
 const ArrowButton = styled.button`
