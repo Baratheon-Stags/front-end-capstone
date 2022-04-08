271d36b00da3908fdb05290442e5b14ec4edf900
JoeVanCamp <vancampjoe@gmail.com>
2022-04-08 12:10:39 -0700

    Your Outfit should persist through page changes via local storage, fixed carousel styling issues

--- client/src/components/yourOutfit/Outfit.jsx
+++ client/src/components/yourOutfit/Outfit.jsx
@@ -1,27 +1,38 @@
 import React, { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
 import axios from 'axios';
 import FlexContainer from '../styled/FlexContainer.styled';
 import StyledCarousel from '../styled/RelatedCarousel.styled';
 import OutfitCard from './OutfitCard';
 import AddItemCard from './AddItemCard';
 
+const SpecialFlex = styled(FlexContainer)`
+  width: 90%;
+`;
+
 const Outfit = ({ productId }) => {
   const carousel = useRef(null);
   const [addedItems, setAddedItems] = useState([]);
-  const [productDetails, setProductDetails] = useState([]);
+  // const [productDetails, setProductDetails] = useState([]);
+  const [productDetails, setProductDetails] = useState(() => {
+    // Fetch locally stored data drive Outfit rendering
+    const arr = [];
+    const keys = Object.keys(localStorage);
+
+    keys.forEach((id) => {
+      const saved = localStorage.getItem(`${id}`);
+      const formatted = JSON.parse(saved);
+      arr.push(formatted);
+    });
+    return arr || [];
+  });
 
   const style = {
     outline: '0',
     fontSize: '25px',
     backgroundColor: 'Transparent',
     border: 'none',
-    margin: '10px'
-  }
-
-  const addItem = () => {
-    if (!addedItems.includes(productId)) {
-      setAddedItems([...addedItems, productId]);
-    }
+    margin: '10px',
   };
 
   useEffect(() => {
@@ -37,6 +48,16 @@ const Outfit = ({ productId }) => {
     }
   }, [addedItems]);
 
+  const snapRight = () => {
+    carousel.current.scrollLeft += 1000;
+  };
+
+  const addItem = () => {
+    if (!addedItems.includes(productId)) {
+      setAddedItems([...addedItems, productId]);
+      snapRight();
+    }
+  };
 
   const scrollRight = () => {
     carousel.current.scrollLeft += 450;
@@ -49,7 +70,7 @@ const Outfit = ({ productId }) => {
   return (
     <>
       <h1>Outfit</h1>
-      <FlexContainer gap="0">
+      <SpecialFlex gap="0">
         <button style={style} type="button" onClick={scrollLeft}>&lt;</button>
         <FlexContainer>
           <AddItemCard addItem={addItem} productId={productId} />
@@ -61,7 +82,7 @@ const Outfit = ({ productId }) => {
           </StyledCarousel>
         </FlexContainer>
         <button style={style} type="button" onClick={scrollRight}>&gt;</button>
-      </FlexContainer>
+      </SpecialFlex>
     </>
   )
 };
